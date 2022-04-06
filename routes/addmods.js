e729c35a4439d1b5917f798e21535389f14db198
Nikhila212 <97923761+Nikhila212@users.noreply.github.com>
2022-04-06 12:40:21 -0500

    updated

--- routes/addmods.js
+++ routes/addmods.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('restaurant', { title: 'Search results Restaurant' });
+router.get('/', function (req, res, next) {
+  let query = req.query
+  console.log(`rows ${query.rows}`)
+  console.log(`cols ${query.cols}`)
+  res.render('addmods', { title: 'Add Mods' , query: query});
 });
 
 module.exports = router;
