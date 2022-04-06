2446e462f8a2937a9ce053c108266743160ebf73
Nikhila212 <97923761+Nikhila212@users.noreply.github.com>
2022-04-06 13:49:48 -0500

    selector.js updated

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var BarbequeRouter = require('./routes/Barbeque');
 var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
 
 
 
@@ -27,6 +28,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Barbeque', BarbequeRouter);
 app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter);
 
 
 
