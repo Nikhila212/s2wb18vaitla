d2fcfb1990f63501ceb2646462fac6632ca22db7
Nikhila212 <97923761+Nikhila212@users.noreply.github.com>
2022-04-06 12:51:46 -0500

    updated index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('restaurant', { title: 'Search results Restaurant' });
+  res.render('barbeque', { title: 'Search results barbeque specials' });
 });
 
 module.exports = router;
