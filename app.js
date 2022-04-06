79ee8dad49d8f80abb2619f120f31b9627179c69
Nikhila212 <97923761+Nikhila212@users.noreply.github.com>
2022-04-06 12:46:18 -0500

    part 6 updated

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var BarbequeRouter = require('./routes/Barbeque');
-var addmodsRouter = require('./routes/addmods');
+
 
 
 
@@ -26,7 +26,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Barbeque', BarbequeRouter);
-app.use('/addmods', addmodsRouter);
 
 
 
